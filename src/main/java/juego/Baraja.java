c03b2ade952ca9e7975da5d90696411d1680da1e
cristinaj1 <cristinacee305@gmail.com>
2022-03-15 16:42:28 +0100

    Clase Baraja comenzada

---
+++ src/main/java/juego/Baraja.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juego;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author Cris
+ */
+public class Baraja {
+
+    private Set<Carta> baraja;
+
+    public Baraja() {
+        this.baraja = new TreeSet<>();
+    }
+
+}
