65e75f0d4cca9fed9c5a02e723ad49eb8f2ae5d6
acutuc <acutuc94@gmail.com>
2022-03-15 16:39:59 +0100

    Actualizada clase Carta, con su estructura general implementada.

--- src/main/java/juego/Carta.java
+++ src/main/java/juego/Carta.java
@@ -1,5 +1,7 @@
 package juego;
 
+import java.util.Objects;
+
 /**
  *
  * @author Gabriel
@@ -11,6 +13,70 @@ public class Carta {
     private int numero;
     
     //Constructor por defecto.
+    public Carta(){
+    }
     
+    //Constructor parametrizado.
+    public Carta(Palos palo, int numero) {
+        if(numero <= 12){
+            this.numero = numero;
+        }
+        this.palo = palo;
+    }
+    
+    //Getters y setters.
+    public Palos getPalo() {
+        return palo;
+    }
+
+    public void setPalo(Palos palo) {
+        this.palo = palo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    //hashCode()
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 29 * hash + Objects.hashCode(this.palo);
+        hash = 29 * hash + this.numero;
+        return hash;
+    }
+
+    //equals().
+    //Una carta será igual a otra si son iguales en PALO y NÚMERO.
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Carta other = (Carta) obj;
+        if (this.numero != other.numero) {
+            return false;
+        }
+        if (this.palo != other.palo) {
+            return false;
+        }
+        return true;
+    }
+
+    //toString().
+    @Override
+    public String toString() {
+        return "Carta{" + "palo=" + palo + ", numero=" + numero + '}';
+    }
     
 }
