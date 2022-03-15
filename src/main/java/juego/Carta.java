317ef472c12f2822b93727bd13ed7c34c4290c98
acutuc <acutuc94@gmail.com>
2022-03-15 16:32:55 +0100

    Se sube clase Carta

---
+++ src/main/java/juego/Carta.java
@@ -0,0 +1,16 @@
+package juego;
+
+/**
+ *
+ * @author Gabriel
+ */
+//Definición de la clase.
+public class Carta {
+    //Atributos de la clase.
+    private Palos palo;
+    private int numero;
+    
+    //Constructor por defecto.
+    
+    
+}
