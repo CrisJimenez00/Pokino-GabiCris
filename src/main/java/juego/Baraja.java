fbcbc4ac42f7a639c9d405af8fb765c98f0b6458
cristinaj1 <cristinacee305@gmail.com>
2022-04-18 18:01:39 +0200

    Método para crear la baraja perfeccionado

--- src/main/java/juego/Baraja.java
+++ src/main/java/juego/Baraja.java
@@ -5,7 +5,6 @@
  */
 package juego;
 
-import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -16,15 +15,31 @@ import java.util.TreeSet;
 public class Baraja {
 
     private Set<Carta> baraja;
+    private final int TAMANIO_BARAJA = 48;
 
     public Baraja() {
         this.baraja = new TreeSet<>();
     }
 
+    //Método el cual rellena la baraja de cartas que no están repetidas.
     public void rellenarBaraja() {
+        //creamos una carta con el constructor predeterminado
         Carta carta = new Carta();
-        for (int i = 0; i < 200; i++) {
-            baraja.add(carta.generarCartaAleatoria());
+
+        //Con el for nos aseguramos que el set al final acabe teniendo un largo de 48
+        for (int i = 0; i < TAMANIO_BARAJA; i++) {
+
+            //generamos la carta de forma aleatoria
+            Carta aleatoria = carta.generarCartaAleatoria();
+
+            //Si no se encuentra en el set lon añade
+            if (!baraja.contains(aleatoria)) {
+                baraja.add(aleatoria);
+
+                //Si no, resta 1 a i y así no se sale del bucle
+            } else {
+                i--;
+            }
         }
 
     }
